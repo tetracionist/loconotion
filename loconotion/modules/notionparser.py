a07142415a6b505b21ebc6149b898613b38e298e
Alexander Wornast <40890820+tetracionist@users.noreply.github.com>
2024-07-02 21:32:54 +0100

    readded headless

--- loconotion/modules/notionparser.py
+++ loconotion/modules/notionparser.py
@@ -232,12 +232,14 @@ class Parser:
 
         chrome_options = Options()
         if not self.args.get("non_headless", False):
+            chrome_options.add_argument("--headless")
             chrome_options.add_argument("window-size=1920,20000")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--log-level=3")
         chrome_options.add_argument("--silent")
         chrome_options.add_argument("--disable-logging")
+        
         #  removes the 'DevTools listening' log message
         chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
         return webdriver.Chrome(
